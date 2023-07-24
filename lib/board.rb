1478861b7a32fdef4e96920147109eaf3dfe79c8
nikasagina <nsagi21@freeuni.edu.ge>
2023-07-24 05:24:17 +0400

    Add under_attack? method for checking square before king moves

--- lib/board.rb
+++ lib/board.rb
@@ -10,9 +10,13 @@ require 'colorize'
 
 class Board
   attr_reader :board
+  attr_accessor :castle_aviable
 
   def initialize
     @board = Array.new(8) { Array.new(8, nil) }
+    @castle_aviable = {}
+    @castle_aviable['white'] = [true, true]
+    @castle_aviable['black'] = [true, true]
   end
 
   def set_piece(piece, location)
@@ -62,5 +66,86 @@ class Board
 
   def is_ocupied?(location)
     board[location[0]][location[1]] != nil
+  end
+
+  def under_attack?(location, color)
+    # Check for Knights
+    knight_moves = [
+      [-2, -1],
+      [-2, 1],
+      [-1, -2],
+      [-1, 2],
+      [1, -2],
+      [1, 2],
+      [2, -1],
+      [2, 1]
+    ]
+    knight_moves.each do |move|
+      new_location = [location[0] + move[0], location[1] + move[1]]
+      if in_bounds?(new_location) && is_ocupied?(new_location) && get_piece(new_location).instance_of?(Knight) && get_piece(new_location).color != color
+        return true
+      end
+    end
+
+    # Check for Rooks and Queens along rows and columns
+    [[-1, 0], [1, 0], [0, -1], [0, 1]].each do |direction|
+      new_location = location.dup
+      loop do
+        new_location = [new_location[0] + direction[0], new_location[1] + direction[1]]
+        break unless in_bounds?(new_location)
+
+        next unless is_ocupied?(new_location)
+
+        piece = get_piece(new_location)
+        return true if piece.color != color && (piece.instance_of?(Rook) || piece.instance_of?(Queen))
+
+        break
+      end
+    end
+
+    # Check for Bishops and Queens along diagonals
+    [[-1, -1], [-1, 1], [1, -1], [1, 1]].each do |direction|
+      new_location = location.dup
+      loop do
+        new_location = [new_location[0] + direction[0], new_location[1] + direction[1]]
+        break unless in_bounds?(new_location)
+
+        next unless is_ocupied?(new_location)
+
+        piece = get_piece(new_location)
+        return true if piece.color != color && (piece.instance_of?(Bishop) || piece.instance_of?(Queen))
+
+        break
+      end
+    end
+
+    # Check for Pawns
+    pawn_direction = color == 'white' ? 1 : -1
+    [[pawn_direction, -1], [pawn_direction, 1]].each do |move|
+      new_location = [location[0] + move[0], location[1] + move[1]]
+      if in_bounds?(new_location) && is_ocupied?(new_location) && get_piece(new_location).instance_of?(Pawn) && get_piece(new_location).color != color
+        return true
+      end
+    end
+
+    # Check for Kings
+    king_moves = [
+      [-1, -1],
+      [-1, 0],
+      [-1, 1],
+      [0, -1],
+      [0, 1],
+      [1, -1],
+      [1, 0],
+      [1, 1]
+    ]
+    king_moves.each do |move|
+      new_location = [location[0] + move[0], location[1] + move[1]]
+      if in_bounds?(new_location) && is_ocupied?(new_location) && get_piece(new_location).instance_of?(King) && get_piece(new_location).color != color
+        return true
+      end
+    end
+
+    false
   end
 end
