6d3b664d606c99533b7b239660ef3c64f1ece6e7
nikasagina <nsagi21@freeuni.edu.ge>
2023-07-24 19:10:12 +0400

    Add coordinates converter

---
+++ lib/coords_converter.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+class CoordsConverter
+  FILE_MAP = {
+    0 => 'a',
+    1 => 'b',
+    2 => 'c',
+    3 => 'd',
+    4 => 'e',
+    5 => 'f',
+    6 => 'g',
+    7 => 'h'
+  }
+
+  def self.to_chess_standard(location)
+    return nil unless location.is_a?(Array) && location.length == 2
+
+    row, col = location
+    return nil unless row.between?(0, 7) && col.between?(0, 7)
+
+    file = FILE_MAP[col]
+    rank = row + 1
+
+    "#{file}#{rank}"
+  end
+
+  def self.to_internal(coord)
+    return nil unless coord.match?(/^[a-h][1-8]$/)
+
+    row = coord[1].to_i - 1
+    col = FILE_MAP.invert[coord[0]]
+
+    [row, col]
+  end
+end
