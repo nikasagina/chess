f01b6b53682df2e39ebfac985032833190abd7e7
nikasagina <nsagi21@freeuni.edu.ge>
2023-07-25 06:22:31 +0400

    Add basic playability

--- lib/main.rb
+++ lib/main.rb
@@ -1,7 +1,34 @@
 # frozen_string_literal: true
 
 require_relative 'board_builder'
+require_relative 'coords_converter'
 
-board = BoardBuilder.new_board
+class Main
+  def initialize
+    @board = BoardBuilder.new_board
+    @current_color = 'white'
+  end
 
-puts board
+  def run
+    loop do
+      puts @board
+      puts "It is #{@current_color}'s turn. Enter a move (e.g., 'e2 e4'):"
+      input = gets.chomp
+      from, to = input.split.map { |coord| CoordsConverter.to_internal(coord) }
+
+      piece = @board.get_piece(from)
+      if piece.nil? || piece.color != @current_color
+        puts 'Invalid move. Please try again.'
+        next
+      end
+
+      if piece.move(to)
+        @current_color = @current_color == 'white' ? 'black' : 'white'
+      else
+        puts 'Invalid move. Please try again.'
+      end
+    end
+  end
+end
+
+Main.new.run
